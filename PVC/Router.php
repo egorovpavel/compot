df7533d5dab7ee28131f7e11becf745dddb387db
Pavel Egorov <pavel@egorov.ru>
2013-10-13 23:05:10 +0100

    Router added

---
+++ PVC/Router.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: pavel_000
+ * Date: 10/13/13
+ * Time: 10:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+
+
+class Router {
+
+}
